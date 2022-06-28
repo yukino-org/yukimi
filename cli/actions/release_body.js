5dec55b863d596d015a6b260cb8f409e759f9900
zyrouge <naveencnaveen96@gmail.com>
2022-06-28 14:27:32 +0530

    ci: release body

---
+++ cli/actions/release_body.js
@@ -0,0 +1,9 @@
+const getReleaseBody = async () => {
+    const { GITHUB_REPOSITORY, RELEASE_TAG } = process.env;
+
+    return `
+Changelogs: https://github.com/${GITHUB_REPOSITORY}/commits/v${RELEASE_TAG}
+`.trim();
+};
+
+module.exports = { getReleaseBody };
